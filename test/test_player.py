34517ebb3beb57e94ca5a463ee1ba77bf9239e76
rfloweree <rubenloweree@gmail.com>
2021-05-04 10:51:41 -0600

    Added test for Player object.

---
+++ test/test_player.py
@@ -0,0 +1,21 @@
+from player import Player
+from unittest import TestCase
+from unittest.mock import patch
+
+
+class TestPlayer(TestCase):
+    def setUp(self):
+        self.player = Player("Alice")
+    
+    def tearDown(self):
+        pass
+
+    def test_get_name(self):
+        self.assertEqual(self.player.get_name(), 'Alice')
+    
+    def test_get_bet(self):
+        self.assertEqual(self.player.get_bet(), [None, None])
+
+    def test_set_bet(self):
+        self.player.set_bet([5, 5])
+        self.assertEqual(self.player.get_bet(), [5, 5])
