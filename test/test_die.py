7ce0e16a1e518a11a39505437e26ce9606a5d095
rfloweree <rubenloweree@gmail.com>
2021-05-04 10:27:42 -0600

    Remove print outs in test.

--- test/test_die.py
+++ test/test_die.py
@@ -10,12 +10,10 @@ class TestDie(TestCase):
         pass
 
     def test_get_face_up(self):
-        print('test_get_face_up')
         self.assertEqual(self.die.get_face_up(), None)
     
     @mock.patch('die.choice', return_value=3, autospec=True)
     def test_roll(self, mock_choice):
-        print('test_roll')
         self.die.roll()
         mock_choice.assert_called_with([1, 2, 3, 4, 5, 6])
         
