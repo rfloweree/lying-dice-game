ff0bc6ccdbfd98995d86234b967738c59bbdcbed
rfloweree <rubenloweree@gmail.com>
2021-05-04 09:22:57 -0600

    Added test for DIe object

---
+++ test/test_die.py
@@ -0,0 +1,19 @@
+from die import Die
+from unittest import mock
+from unittest import TestCase
+
+class TestDie(TestCase):
+    def setUp(self):
+        self.die = Die()
+    
+    def tearDown(self):
+        pass
+
+    def test_get_face_up(self):
+        self.assertEqual(self.die.get_face_up(), None)
+    
+    @mock.patch('die.choice', return_value=3, autospec=True)
+    def test_roll(self, mock_choice):
+        self.die.roll()
+        mock_choice.assert_called_with([1, 2, 3, 4, 5, 6])
+        
