b6568f2ca3a3d5b2872c42b4a869ebabdeb470bd
rfloweree <rubenloweree@gmail.com>
2021-05-06 14:08:28 -0600

    Created the dice rolling object.

---
+++ dice_roll.py
@@ -0,0 +1,11 @@
+from random import seed
+from random import choice
+import logging
+
+logger = logging.getLogger(__name__)
+
+seed()
+faces =[1, 2, 3, 4, 5, 6]
+
+def roll_Die():
+    return choice(faces)
