2e8ab05bd26d160c6bffef75d3d5f65565a1c1ab
rfloweree <rubenloweree@gmail.com>
2021-05-05 22:10:52 -0600

    Added start function to game object

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print('Hello World, agian!')
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Game:
+    def __init__(self, players):
+        self.players = players
+        logger.debug('Init game')
+    
+    def start(self):
+        pass
