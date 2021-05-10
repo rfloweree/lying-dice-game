614ef6476e14e4fb2196b10717147250919db776
rfloweree <rubenloweree@gmail.com>
2021-05-10 17:26:59 -0600

    Updated Test cases

--- test/test_die.py
+++ test/test_die.py
@@ -12,9 +12,7 @@ class TestDie(TestCase):
     def test_get_face_up(self):
         self.assertEqual(self.die.get_face_up(), None)
     
-    # @mock.patch('die.choice', return_value=3, autospec=True)
     def test_roll(self):
         self.die.roll(3)
         self.assertEqual(self.die.get_face_up(), 3)
-        # mock_choice.assert_called_with([1, 2, 3, 4, 5, 6])
         
