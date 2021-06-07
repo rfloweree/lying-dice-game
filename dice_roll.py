0ce25271072e20e304f398a84f48257474fa4253
rfloweree <rubenloweree@gmail.com>
2021-06-07 09:34:03 -0600

    Added multiple dice roll and tests.

--- dice_roll.py
+++ dice_roll.py
@@ -5,10 +5,31 @@ import logging
 logger = logging.getLogger(__name__)
 
 seed()
-faces =[1, 2, 3, 4, 5, 6]
+faces = [1, 2, 3, 4, 5, 6]
 
 def roll_Die():
+    '''
+    Rolls a single six-sided die.
+    Returns the integer value of thh side facing up.
+    '''
     return choice(faces)
 
 def roll_dice():
+    '''
+    Rolls two six-sided dice.
+    Returns a tuple of the integer value of the side facing up for each die.
+    '''
     return (choice(faces), choice(faces))
+
+def roll_dice_poly(number_faces, number_dice):
+    '''
+    Rolls a given number of n-sided dice. 
+    Returns a list of the integer value of the side facing up for each die.
+    '''
+    dice_faces = []
+    dice_rolls = []
+    for num in range(1, number_faces+1):
+        dice_faces.append(num)
+    for dice in  range(number_dice):
+        dice_rolls.append(choice(dice_faces))
+    return dice_rolls
--- test/test_dice_roll.py
+++ test/test_dice_roll.py
@@ -11,6 +11,17 @@ class TestDice_Roll(TestCase):
         pass
 
     @mock.patch('dice_roll.choice', return_value=3, autospec=True)
-    def test_roll_dice(self, mock_choice):
+    def test_roll_die(self, mock_choice):
         self.assertEqual(dice_roll.roll_Die(), 3)
         mock_choice.assert_called_with([1, 2, 3, 4, 5, 6])
+    
+    @mock.patch('dice_roll.choice', return_value=5, autospec=True)
+    def test_roll_dice(self, mock_choice):
+        self.assertEqual(dice_roll.roll_dice(), (5, 5))
+        mock_choice.assert_called_with([1, 2, 3, 4, 5, 6])
+
+    @mock.patch('dice_roll.choice', return_value=4, autospec=True)
+    def test_roll_dice_poly(self,mock_choice):
+        self.assertEqual(dice_roll.roll_dice_poly(4, 3), [4, 4, 4])
+        mock_choice.assert_called_with([1, 2, 3, 4])
+        self.assertEqual(dice_roll.roll_dice_poly(4, 6), [4, 4, 4, 4, 4, 4])
