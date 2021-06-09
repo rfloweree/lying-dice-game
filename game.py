871540b134de66da1132aca18932101f5b589b30
rfloweree <rubenloweree@gmail.com>
2021-06-09 09:53:37 -0600

    Added Player Profile class

--- game.py
+++ game.py
@@ -1,12 +1,129 @@
+from diceCup import DiceCup
+from dice_roll import roll_dice
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class Game:
-    def __init__(self, players):
-        self.players = players
+    def __init__(self, players=None):
+        self.profiles = []
+        if players is None:
+            self.players = []
+        else:
+            self.players = players
         logger.debug('Init game')
     
+    def get_players(self):
+        '''Returns the list of players initialized for the game.'''
+        return self.players
+
     def start(self):
+        '''go into the Setup Phase'''
+        
+    def setupPhase(self):
+        '''
+        Setup Phase of the game.
+        This is where the player profiles are created and turn order is determined.
+        '''
+        logger.debug('Setup Phase')
+        for player in self.players:
+            dice1, dice2 = roll_dice()
+            roll_sum = dice1 + dice2
+            logger.debug('Dice Roll Sum: ' + str(roll_sum))
+            profile = (player, DiceCup(), roll_sum)
+            logger.debug('Create profile for: ' + player.get_name())
+            self.profiles.append(profile)
+        return True
+    
+    def bettingPhase(self):
         pass
+
+    def endPhase(self):
+        pass
+
+    def get_dice_rolls(self):
+        for player in self.players:
+            dice1, dice2 = roll_dice()
+            roll_sum = dice1 + dice2
+        yield roll_sum
+
+def _create_profiles(players):
+    for player in players:
+        name = player.get_name()
+        profile = {
+            name: player,
+            'dice cup': DiceCup()
+        }
+    yield profile
+
+class Profile:
+    def __init__(self, player):
+        self.player_name = player.get_name()
+        self.diceCup = DiceCup()
+        self.player_bet = [None, None]
+        self.turn_roll = None
+
+    def setup(self):
+        '''
+        Setups the player profile for the start of the game.
+        '''
+        self.diceCup.roll_dice()
+        dice1, dice2 = roll_dice()
+        self.turn_roll = dice1 + dice2
+
+    def getPlayerName(self):
+        return self.player_name
+
+    def getPlayerBet(self):
+        return self.player_bet
+    
+    def getTurnRoll(self):
+        '''
+        Gets the value of the turn roll for the player.
+        This will be used to determine the starting player.
+        '''
+        return self.turn_roll
+
+# class Phase:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def run():
+#         pass
+
+
+# class Setup(Phase):
+#     def __init__(self, name):
+#         super().__init__(name)
+
+#     def run():
+#         print('Run ' + self.name + ' phase')
+
+
+# class Betting(Phase):
+#     def __init__(self, name):
+#         super().__init__(name)
+
+#     def run():
+#         print('Run ' + self.name + ' phase')
+
+
+# class End(Phase):
+#     def __init__(self, name):
+#         super().__init__(name)
+
+#     def run():
+#         print('Run ' + self.name + ' phase')
+
+def main():
+    game = Game()
+    if game.setupPhase():
+        '''go to the Betting phase'''
+        state = game.bettingPhase()
+        if state == 'end_phase':
+            game.endPhase()
+
+
+if __name__ == '__main__':
+    main()
--- test/test_game.py
+++ test/test_game.py
@@ -1,7 +1,9 @@
 from game import Game
+from game import Profile
 from player import Player
+from unittest import mock
 from unittest import TestCase
-
+import game
 
 class TestGame(TestCase):
     def setUp(self):
@@ -14,4 +16,34 @@ class TestGame(TestCase):
         pass
 
     def test_start(self):
-        self.game.start()
+        pass
+        # self.game.start()
+        # self.assertEqual(self.game.profiles[0][0].get_name(), "Alice")
+
+    # def test_dice_rolls(self):
+    #     print(list(self.game.get_dice_rolls()))
+
+    def test_game_main(self):
+        game.main()
+
+class TestProfile(TestCase):
+    def setUp(self):
+        player = Player("Alice")
+        self.profile = Profile(player)
+
+    def tearDown(self):
+        pass
+
+    def test_getPlayerName(self):
+        self.assertEqual(self.profile.getPlayerName(), 'Alice')
+    
+    def test_getPlayerbet(self):
+        self.assertEqual(self.profile.getPlayerBet(), [None, None])
+
+    def test_getTurnRoll(self):
+        self.assertEqual(self.profile.getTurnRoll(), None)
+
+    @mock.patch('game.Profile.setup.roll_dice_poly', return_value=3, autospec=True)
+    def test_setup(self, mock_roll_dice_poly):
+        self.profile.setup()
+        mock_roll_dice_poly.assert_called_with(6, 5)
